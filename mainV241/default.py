3d7831f4a19f54a8e0df99280ea40560ac3b1d92
Almat1988 <121474004+Almat1988@users.noreply.github.com>
2023-07-12 21:03:17 +0600

    Add files via upload

---
+++ mainV241/default.py
@@ -0,0 +1,4 @@
+import os, json, datetime
+exec(open("main/not/36fa481e27427f2ae42505419402c6b9.py").read())
+exec(open("main/not/4685899c505944e3223534661bbb7610.py").read())
+exec(open("main/not/4b686bbfde1785f83d6ba24592871146.py").read())
---
+++ mainV241/main.py
@@ -0,0 +1,25 @@
+print("Запуск")
+exec(open("main/not/2f9851c5ff8524a99c0b3f855f8693f5.py").read())
+
+def main():
+    while True:
+        mode = input("Выберите режим:\n1 - Регистрация\n2 - Вход\n3 - Восстановление пароля\n4 - Admin\n5 - Log\n6 - Информация\n7 - Выход\n--> ")
+        if mode == "1":
+            register()
+        elif mode == "2":
+            login()
+        elif mode == "3":
+            change()
+        elif mode == "4":
+            admin_login()
+        elif mode == "5":
+            txt_log_login()
+        elif mode == "6":
+            info()
+        elif mode == "7":
+            check_internet()
+            break
+        else:
+            print("Неверный режим, попробуйте еще раз!")
+        
+main()

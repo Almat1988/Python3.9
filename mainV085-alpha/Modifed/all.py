69c7c1297f1e0c559192f25eb70da47c100aa2e5
Almat1988 <121474004+Almat1988@users.noreply.github.com>
2023-07-12 20:40:43 +0600

    Create all.py

---
+++ mainV085-alpha/Modifed/all.py
@@ -0,0 +1,8 @@
+exec(open("main/not/modules.py").read())
+exec(open("main/not/create.py").read())
+exec(open("main/not/setting.py").read())
+exec(open("main/not/log.py").read())
+exec(open("main/default.py").read())
+exec(open("main/not/view_log.py").read())
+exec(open("main/not/admin.py").read())
+exec(open("main/not/telegram.py").read())

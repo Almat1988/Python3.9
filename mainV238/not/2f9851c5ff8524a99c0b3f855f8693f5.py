64637eb20a3b357847dc14d474317c8387ee1eb0
Almat1988 <121474004+Almat1988@users.noreply.github.com>
2023-07-12 20:57:57 +0600

    Create 2f9851c5ff8524a99c0b3f855f8693f5.py

---
+++ mainV238/not/2f9851c5ff8524a99c0b3f855f8693f5.py
@@ -0,0 +1,30 @@
+import os, json, datetime, hashlib, string, random, telegram, asyncio
+
+import urllib.request, time
+
+if not os.path.exists("Admin"):
+    os.mkdir("Admin")
+
+if not os.path.exists("Admin/log"):
+    os.mkdir("Admin/log")
+
+exec(open("main/not/4685899c505944e3223534661bbb7610.py").read())
+
+if not os.path.exists(file_log_register):
+    open(file_log_register, "w").close()
+if not os.path.exists(file_log_login):
+    open(file_log_login, "w").close()
+if not os.path.exists(file_log_change):
+    open(file_log_change, "w").close()
+if not os.path.exists(file_log_pass):
+    open(file_log_pass, "w").close()
+if not os.path.exists(file_users):
+    with open(file_users, "w") as f:
+        json.dump({}, f)
+
+exec(open("main/not/36fa481e27427f2ae42505419402c6b9.py").read())
+exec(open("main/not/4b686bbfde1785f83d6ba24592871146.py").read())
+exec(open("main/not/ac7c3442bd68c7e89fddec15fcc750d0.py").read())
+exec(open("main/not/a8724a7564467f6d64902e3dec7f8193.py").read())
+exec(open("main/not/51546b49f9d9d55e25e3e5f453168a5b.py").read())
+exec(open("main/not/94f6217968556f347f3a29f7e046236c.py").read())

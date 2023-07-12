26199391e4efb1099313d03e64cecc4008940119
Almat1988 <121474004+Almat1988@users.noreply.github.com>
2023-07-12 20:58:40 +0600

    Add files via upload

---
+++ mainV238/not/36fa481e27427f2ae42505419402c6b9.py
@@ -0,0 +1,15 @@
+def log_register(username, success):
+    with open(file_log_register, "a") as file:
+        file.write(f"REGISTER DATE={datetime.datetime.now()}; \nuser=({username}): {'УСПЕХ' if success else 'ОШИБКА'}\n\n")
+
+def log_login(username, success):
+    with open(file_log_login, "a") as file:
+        file.write(f"LOGIN DATE={datetime.datetime.now()}; \nuser=({username}): {'УСПЕХ' if success else 'ОШИБКА'}\n\n")
+
+def log_change(username, new_password, success):
+    with open(file_log_change, "a") as file:
+        file.write(f"CHANGE DATE={datetime.datetime.now()}; \nuser=({username});  new_pass=({new_password}): {'УСПЕХ' if success else 'ОШИБКА'}\n\n")
+
+def log_pass(username, password, code, success):
+    with open(file_log_pass, "a") as file:
+        file.write(f"PASS DATE={datetime.datetime.now()}; \nuser=({username}); pass=({password}); code=({code}): {'УСПЕХ' if success else 'ОШИБКА'}\n\n")
---
+++ mainV238/not/94f6217968556f347f3a29f7e046236c.py
@@ -0,0 +1,8 @@
+MAIN_VERSION = "2.3.8"
+AUTHOR = "aimt(aimtYT)"
+YOUTUBE = "https://youtube.com/@aimt"
+TELEGRAM = "https://t.me/+HMSvsI20gog4Mjcy"
+INFO = "English\n1) Error corrections\n2) Addition of shortcomings\n3) Addition of protection\nРуский\n1) Исправлений ошибок\n2) Добавление недостаток\n3) Добавление защиты"
+
+def info():
+    print(f"MAIN Version: {MAIN_VERSION}\nAuthor: {AUTHOR}\nYouTube: {YOUTUBE}\nTelegram: {TELEGRAM}\nInfo: {INFO}")
